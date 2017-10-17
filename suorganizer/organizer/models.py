dca585d3ccbbfc774a531bdd34186fb1bd440fe7
xuhui <zmm064@foxmail.com>
2017-10-17 21:20:51 +0800

    完成第二章定义好了模型类

--- suorganizer/organizer/models.py
+++ suorganizer/organizer/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name         = models.CharField(max_length=31, unique=True)
+    slug         = models.SlugField(max_length=31, unique=True, help_text='A label for URL config')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Startup(models.Model):
+    name         = models.CharField(max_length=31, db_index=True)
+    slug         = models.SlugField(max_length=31, unique=True, help_text='A label for URL config')
+    description  = models.TextField()
+    founded_date = models.DateField(verbose_name='date founded')
+    contact      = models.EmailField()
+    website      = models.URLField(max_length=255)
+
+    tags         = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        get_latest_by = 'founded_date'
+
+
+class NewsLink(models.Model):
+    title        = models.CharField(max_length=63)
+    pub_date     = models.DateField(verbose_name='date published')
+    link         = models.URLField(max_length=255)
+
+    startup      = models.ForeignKey(Startup)
+
+    def __str__(self):
+        return "{}:{}".format(self.startup, self.title)
+
+    class Meta:
+        verbose_name  = 'news article'
+        ordering      = ['-pub_date']
+        get_latest_by = 'pub_date'
+
--- suorganizer/suorganizer/settings.py
+++ suorganizer/suorganizer/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
+    'organizer',
 ]
 
 MIDDLEWARE_CLASSES = [
