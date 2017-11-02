4f25a254f5f5148eb4130e1bbf4951cf376e4619
xuhui <zmm064@foxmail.com>
2017-11-02 10:26:03 +0800

    加了点样式

--- suorganizer/suorganizer/settings.py
+++ suorganizer/suorganizer/settings.py
@@ -123,5 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+#STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
